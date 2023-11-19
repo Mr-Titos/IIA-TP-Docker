a8ef8b414f31af744c7dbd338a4a7034d6e30ad6
Mr Titos <arthur.titos2@gmail.com>
2023-11-19 18:00:51 +0100

    initialization v1

---
+++ BUILDER/tp_back/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require('cors');
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+  }
+);
+
+const Jeux = sequelize.define("Jeux", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+  },
+  nom: DataTypes.STRING,
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await Jeux.sync();
+    await Jeux.findOrCreate({
+      where: { id: 1 },
+      defaults: {
+        nom: "Elden Ring",
+      },
+    });
+    await Jeux.findOrCreate({
+      where: { id: 2 },
+      defaults: {
+        nom: "Stellaris",
+      },
+    });
+    await Jeux.findOrCreate({
+      where: { id: 3 },
+      defaults: {
+        nom: "Superman 64",
+      },
+    });
+    console.log("Database initialized successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+const app = express();
+app.use(cors())
+const port = process.env.BACK_PORT || 3000;
+
+app.get("/api", (req, res) => {
+  Jeux.findAll().then(r => res.send(r));
+});
+
+app.listen(port, () => {
+  return console.log(`Server is listening at http://localhost:${port}`);
+});
